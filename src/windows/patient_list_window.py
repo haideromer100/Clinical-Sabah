1367896ed6c71a34f24c7cc39c03670528ba3609
haideromer100 <HAIDER.OMER101@GMAIL.COM>
2025-07-18 03:23:18 +0300

    Remove unused imports

--- src/windows/patient_list_window.py
+++ src/windows/patient_list_window.py
@@ -4,14 +4,7 @@ from __future__ import annotations
 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import (
-    QHBoxLayout,
-    QLineEdit,
-    QTableView,
-    QVBoxLayout,
-    QWidget,
-)
+from PyQt5.QtWidgets import QLineEdit, QTableView, QVBoxLayout, QWidget
 
 from ..database.database_manager import get_connection
 
